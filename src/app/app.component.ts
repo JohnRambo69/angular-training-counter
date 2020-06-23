f41133625b31a9464b7ee193b4184b7a7d0323a5
JohnRambo69 <50015050+JohnRambo69@users.noreply.github.com>
2020-06-23 17:54:31 +0200

    upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title: string = 'Counter App';
+
+  private count: number = 0;
+
+  handleIncrease() {
+    if(this.count < 10) {
+      this.count ++;
+    }
+    
+  }
+
+  handleDecrease() {
+    if(this.count > 0) {
+      this.count --;
+    }
+    
+  }
+
+  handleReset() {
+    this.count = 0;
+  }
+
+  getCount(): number {
+    return this.count;
+  }
+
+}
